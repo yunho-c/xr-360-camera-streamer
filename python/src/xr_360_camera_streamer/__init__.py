eb1be1e3dd7aea6a29bb4c9da3e73ae2d161e173
Yunho Cho <yhjc2692@gmail.com>
2025-07-13 04:13:49 -0400

    Fixed import errors

--- python/src/xr_360_camera_streamer/__init__.py
+++ python/src/xr_360_camera_streamer/__init__.py
@@ -1,7 +1,8 @@
 """A Python project that streams 360 panoramic videos to XR headsets."""
 
-from .__about__ import __version__
+from . import __about__
 from .sources.base import VideoSource
 from .transforms.base import VideoTransform
 
 __all__ = ["__version__", "VideoSource", "VideoTransform"]
+__version__ = __about__.version
